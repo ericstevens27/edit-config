31359cd02ea39820442346f01a1393d90a2d75a6
ericstevens <eric@ericstevens.ca>
2017-03-16 15:33:27 -0400

    Base insertion of Disable Apps Button, Hide Google Bar, App list, Reserved areas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Edit Launcher Configuration';
+  quoteString = '&quot;';
+  bracketOpen = '{';
+  bracketClose = '}';
+  disableAppsButton = false;
+  hideGoogleSearchBar = false
+  appFilterList = [];
+  appToAdd = '';
+  resArea = [
+    [ false, false, false, false],
+    [ false, false, false, false],
+    [ false, false, false, false],
+    [ false, false, false, false],
+  ];
+  resHotSeat = [ false, false, false, false, false ]
+
+  onKey(event: any) { // without type info
+    this.appFilterList.push(this.appToAdd)
+    this.appToAdd = ''
+  }
+  onRemoveApp(app: string): void {
+      const index = this.appFilterList.indexOf(app);
+      this.appFilterList.splice(index, 1);
+  }
 }
